17306ffd0bd8e0c9329155b984c98c6c51754d9c
Chase Lybbert <chase.lybbert@mckesson.com>
2019-02-12 11:15:08 -0700

    Changed Request to Response

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {Http, Request} from '@angular/http';
+import {Http, Response} from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
